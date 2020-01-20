b0dee4129bbaca8c922ec458b1a7f22a3bb5a188
kanKda <apiecem9@gmail.com>
2020-01-20 20:28:54 +0400

    scrollbar thumb hotfix

--- src/scripts/scrollbar.js
+++ src/scripts/scrollbar.js
@@ -6,22 +6,31 @@ function initScrollbar() {
     const scrollbar = document.getElementById('scrollbar');
     let initFlag = hiddenZone > 0 ? 1 : 0;
 
+    let thumbHeight;
     if (initFlag) {
-        thumb.style.height = `${scrollbar.offsetHeight - hiddenZone}px`;
+        thumbHeight = (scrollbar.offsetHeight - 
+            (scrollbar.offsetHeight / Math.round(hiddenZone / 10))) 
+            / Math.round(hiddenZone / 10);
+        thumbHeight = Math.round(thumbHeight);
+        thumb.style.height = `${thumbHeight}px`;
         scrollbar.style.visibility = 'visible';
     }
 
     const max = Math.round(hiddenZone / 10) * 10;
+    let thumbStep = (scrollbar.offsetHeight - thumbHeight) / (max / 10);
     let transformCounter = 0;
     let zoneCounter = 0;
+    let iterCounter = 0;
     function scroll(ctx, unit) {
-        if (!ctx && zoneCounter < max) {
-            transformCounter -= unit;
+        if (!ctx && iterCounter < max) {
+            iterCounter += 10;
+            transformCounter -= 10;
             zoneCounter += unit;
             scrollContainer.style.transform = `translateY(${transformCounter}px)`;
             thumb.style.top = `${zoneCounter}px`;
-        } else if (ctx && zoneCounter > 0) {
-            transformCounter += unit;
+        } else if (ctx && iterCounter > 0) {
+            iterCounter -= 10;
+            transformCounter += 10;
             zoneCounter -= unit;
             scrollContainer.style.transform = `translateY(${transformCounter}px)`;
             thumb.style.top = `${zoneCounter}px`;
@@ -31,7 +40,7 @@ function initScrollbar() {
     scrollContainer.addEventListener('mouseover', function() {
         scrollContainer.onwheel = function(event) {
             const ctx = event.deltaY < 0 ? 1 : 0;
-            scroll(ctx, 10);
+            scroll(ctx, thumbStep);
         }
     })
 
@@ -40,12 +49,12 @@ function initScrollbar() {
         let tmpY = event.clientY;
         document.onselectstart = () => false;
         document.onmousemove = function(event) {
-            if (event.clientY >= tmpY + 10) {
-                scroll(0, 10);
-                tmpY += 10;
-            } else if (event.clientY <= tmpY - 10) {
-                scroll(1, 10);
-                tmpY -= 10;
+            if (event.clientY >= tmpY + thumbStep) {
+                scroll(0, thumbStep);
+                tmpY += thumbStep;
+            } else if (event.clientY <= tmpY - thumbStep) {
+                scroll(1, thumbStep);
+                tmpY -= thumbStep;
             }
         }
 
